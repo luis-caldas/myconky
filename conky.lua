cfcded22fee9c8be5a01579003514853948634fc
Luis <admin@luiscaldas.com>
2022-02-28 19:04:29 +0000

    updated conky to work on high dpi

--- conky.lua
+++ conky.lua
@@ -1,148 +1,176 @@
+-- {{{ HiDPI
+
+local function get_scaler()
+	return tonumber(os.getenv("GDK_SCALE"))
+end
+
+local scaler = get_scaler()
+
+-- }}}
+-- {{{ Variable
+
 -- [[ Dev variables ]]
+
 local bar = {
-    length = 100,
-    char = "-"
-}
-local graphs = {
-    per_line = 4,
-    height = 50,
-    width = 200
-}
-local battery_path = {
-    base = "/sys/class/power_supply",
-    variants = {"rk-bat", "BAT0", "BAT1", "BAT2"},
-    has_health = nil
+	length = 100,
+	char = "-"
 }
 
-conky.config = {
-    alignment = 'top_right',
-    -- xinerama_head = 1, -- not needed anymore
-                          -- the monitor can be chosen with the flag '-m'
-    background = false,
-    border_width = 20,
-    cpu_avg_samples = 2,
-    default_bar_height = graphs.height,
-    default_color = 'black',
-    default_outline_color = 'black',
-    double_buffer = true,
-    draw_borders = false,
-    draw_graph_borders = true,
-    draw_outline = false,
-    draw_shades = false,
-    extra_newline = false,
-    default_color = 'ffffff',
-    font = 'Mono:size=12',
-    gap_x = 50,
-    gap_y = 75,
-    minimum_height = 5,
-    minimum_width = 5,
-    net_avg_samples = 2,
-    no_buffers = true,
-    out_to_console = false,
-    out_to_ncurses = false,
-    out_to_stderr = false,
-    out_to_x = true,
-    own_window = true,
-    own_window_transparent = false,
-    own_window_type = 'override',
-    own_window_colour = '0D0D0D',
-    own_window_argb_visual = true,
-    own_window_argb_value = 210,
-    show_graph_range = false,
-    show_graph_scale = false,
-    stippled_borders = 0,
-    update_interval = 1,
-    uppercase = false,
-    use_spacer = 'none',
-    use_xft = true,
-    lua_load = '~/.config/conky/functions.lua'
+local graphs = {
+	per_line = 4,
+	height = 50 * scaler,
+	width = 200 * scaler
 }
+
+local battery_path = {
+	base = "/sys/class/power_supply",
+	variants = {"rk-bat", "BAT0", "BAT1", "BAT2"},
+	has_health = nil
+}
+
+-- }}}
+-- {{{ Functions
 
 -- [[ Command execution function ]]
 local function run_command (command_string)
-    return io.popen(command_string):read('*a'):sub(1, -2)
+	return io.popen(command_string):read('*a'):sub(1, -2)
 end
 
 -- [[ Check if the system has battery ]]
 local function has_battery()
 
-    -- Iterate the paths and check for a valid battery
+	-- Iterate the paths and check for a valid battery
 	for index, bat_var in pairs(battery_path.variants) do
-        if run_command("test -d " .. battery_path.base .. "/" .. bat_var .. " && echo true || echo false") == "true" then
-            battery_path.has_health = run_command("test -f " .. battery_path.base .. "/" .. bat_var .. "/" .. "health" .. " && echo true || echo false") == "true"
-            return index
-        end
+		if run_command("test -d " .. battery_path.base .. "/" .. bat_var .. " && echo true || echo false") == "true" then
+			battery_path.has_health = run_command("test -f " .. battery_path.base .. "/" .. bat_var .. "/" .. "health" .. " && echo true || echo false") == "true"
+			return index
+		end
 	end
 
-    -- If no battery was found return null
-    return 0
+	-- If no battery was found return null
+	return 0
 end
 
 -- [[ String interpolation function ]]
 local function string_interpolation (string_input, variable_table)
-    return string_input:gsub('(#%b{})', function (variable_name)
-        return variable_table[variable_name:sub(3, -2)] or variable_name
-    end)
+	return string_input:gsub('(#%b{})', function (variable_name)
+		return variable_table[variable_name:sub(3, -2)] or variable_name
+	end)
 end
 
 -- [[ String string function ]]
 function string:split(sep)
-    local sep, fields = sep or ":", {}
-    local pattern = string.format("([^%s]+)", sep)
-    self:gsub(pattern, function(c) table.insert(fields, c) end)
+	local sep, fields = sep or ":", {}
+	local pattern = string.format("([^%s]+)", sep)
+	self:gsub(pattern, function(c) table.insert(fields, c) end)
 	return fields
 end
 
+-- }}}
+-- {{{ Config
+
+local function script_path()
+	local str = debug.getinfo(2, "S").source:sub(2)
+	return str:match("(.*/)")
+end
+
+local my_functions = script_path() .. 'functions.lua'
+
+conky.config = {
+	alignment = 'top_right',
+	background = false,
+	border_width = 20 * scaler,
+	cpu_avg_samples = 2,
+	default_bar_height = graphs.height,
+	default_color = 'black',
+	default_outline_color = 'black',
+	double_buffer = true,
+	draw_borders = false,
+	draw_graph_borders = true,
+	draw_outline = false,
+	draw_shades = false,
+	extra_newline = false,
+	default_color = 'ffffff',
+	font = 'Mono:size=12',
+	gap_x = 50 * scaler,
+	gap_y = 75 * scaler,
+	minimum_height = 5 * scaler,
+	minimum_width = 5 * scaler,
+	net_avg_samples = 2,
+	no_buffers = true,
+	out_to_console = false,
+	out_to_ncurses = false,
+	out_to_stderr = false,
+	out_to_x = true,
+	own_window = true,
+	own_window_transparent = false,
+	own_window_type = 'override',
+	own_window_colour = '0D0D0D',
+	own_window_argb_visual = true,
+	own_window_argb_value = 210,
+	show_graph_range = false,
+	show_graph_scale = false,
+	stippled_borders = 0,
+	update_interval = 1,
+	uppercase = false,
+	use_spacer = 'none',
+	use_xft = true,
+	lua_load = my_functions
+}
+
+-- }}}
+-- {{{ Main
+
 -- [[ Pregenerate the needed variables ]] --
 local pregenerated = {
-    release = run_command("lsb_release -sd"):gsub("\"", ""),
-    release_version = run_command("lsb_release -s"):gsub("\"", ""),
-    bash_version = run_command("bash -c 'echo $BASH_VERSION'"),
-    cpu_name = run_command("grep 'model name' /proc/cpuinfo | uniq | cut -f 2 -d ':' | awk '{$1=$1}1'")
+	release = run_command("lsb_release -sd"):gsub("\"", ""),
+	release_version = run_command("lsb_release -s"):gsub("\"", ""),
+	bash_version = run_command("bash -c 'echo $BASH_VERSION'"),
+	cpu_name = run_command("grep 'model name' /proc/cpuinfo | uniq | cut -f 2 -d ':' | awk '{$1=$1}1'")
 }
 
 -- [[ Generate the graph web of CPUs ]]
 local cpu_count = run_command("grep -c ^processor /proc/cpuinfo")
 local cpu_web_string = ""
 for i = 0, cpu_count - 1, graphs.per_line do
-    -- First part generates all the naming
-    for j = 0, graphs.per_line - 1, 1 do
-        if j + i > cpu_count - 1 then break end
-        local core_desc = "Core " ..  i + j + 1
-        cpu_web_string = cpu_web_string .. core_desc
-        if j ~= (graphs.per_line - 1) then
-            cpu_web_string = cpu_web_string .. string.rep(" ", 25 - core_desc:len())
-        end
-    end
-    cpu_web_string = cpu_web_string .. "\n"
+	-- First part generates all the naming
+	for j = 0, graphs.per_line - 1, 1 do
+		if j + i > cpu_count - 1 then break end
+		local core_desc = "Core " ..  i + j + 1
+		cpu_web_string = cpu_web_string .. core_desc
+		if j ~= (graphs.per_line - 1) then
+			cpu_web_string = cpu_web_string .. string.rep(" ", 25 - core_desc:len())
+		end
+	end
+	cpu_web_string = cpu_web_string .. "\n"
 
-    -- Second part lists all the governors
-    for j = 0, graphs.per_line - 1, 1 do
-        if j + i > cpu_count - 1 then break end
-        local gover_desc = "${lua justify_gover " .. i + j + 1 .. " 25}"
-        cpu_web_string = cpu_web_string .. gover_desc
-    end
-    cpu_web_string = cpu_web_string .. "\n"
+	-- Second part lists all the governors
+	for j = 0, graphs.per_line - 1, 1 do
+		if j + i > cpu_count - 1 then break end
+		local gover_desc = "${lua justify_gover " .. i + j + 1 .. " 25}"
+		cpu_web_string = cpu_web_string .. gover_desc
+	end
+	cpu_web_string = cpu_web_string .. "\n"
 
-    -- Third part generates all the frequencies
-    for j = 0, graphs.per_line - 1, 1 do
-        if j + i > cpu_count - 1 then break end
-        -- Use custom frequency command
-        local freq_desc = "${lua justify_core " .. i + j + 1 .. " 25}"
-        cpu_web_string = cpu_web_string .. freq_desc
-    end
-    cpu_web_string = cpu_web_string .. "\n"
+	-- Third part generates all the frequencies
+	for j = 0, graphs.per_line - 1, 1 do
+		if j + i > cpu_count - 1 then break end
+		-- Use custom frequency command
+		local freq_desc = "${lua justify_core " .. i + j + 1 .. " 25}"
+		cpu_web_string = cpu_web_string .. freq_desc
+	end
+	cpu_web_string = cpu_web_string .. "\n"
 
-    -- Fourth part generates all the actual graphs
-    for j = 0, graphs.per_line - 1, 1 do
-        if j + i > cpu_count - 1 then break end
-        cpu_web_string = cpu_web_string .. "${cpugraph" .. " " .. "cpu" .. i + j + 1 .. " " .. graphs.height .. "," .. graphs.width .. "}"
-    end
-    cpu_web_string = cpu_web_string .. "\n"
+	-- Fourth part generates all the actual graphs
+	for j = 0, graphs.per_line - 1, 1 do
+		if j + i > cpu_count - 1 then break end
+		cpu_web_string = cpu_web_string .. "${cpugraph" .. " " .. "cpu" .. i + j + 1 .. " " .. graphs.height .. "," .. graphs.width .. "}"
+	end
+	cpu_web_string = cpu_web_string .. "\n"
 end
 
 -- Final part generates the all cores graph
-cpu_web_string = cpu_web_string .. "All Cores\n" .. "${lua gover_all}\n" .. "${lua conky_justify_core 0 25}" .. "\n"
+cpu_web_string = cpu_web_string .. "All Cores\n" .. "${lua gover_all}\n" .. "${lua justify_core 0 25}" .. "\n"
 cpu_web_string = cpu_web_string .. "${cpugraph cpu0 " .. graphs.height .. "," .. graphs.width * graphs.per_line .. "}"
 
 -- [[ Memory graph string ]]
@@ -154,29 +182,29 @@ mem_web_string = mem_web_string .. "${memgraph " .. graphs.height .. "," .. grap
 bat_web_string = ""
 local battery_nr = has_battery()
 if battery_nr > 0 then
-    battery_full_path = battery_path.base .. "/" .. battery_path.variants[battery_nr]
-    bat_web_string = bat_web_string .. "\n\n" .. bar.char:rep(bar.length) .. "\n"
-    bat_web_string = bat_web_string .. "\nBattery\n"
-    bat_web_string = bat_web_string .. "${exec cat " .. battery_full_path .. "/status" .. "}"
-    if battery_path.has_health then
-        bat_web_string = bat_web_string .. " / "
-        bat_web_string = bat_web_string .. "${exec cat " .. battery_full_path .. "/health" .. "}"
-    end
-    bat_web_string = bat_web_string .. " -- "
-    bat_web_string = bat_web_string .. "${exec cat " .. battery_full_path .. "/capacity" .. "} %\n"
-    bat_web_string = bat_web_string .. "${execbar cat " .. battery_full_path .. "/capacity" .. "}"
+	battery_full_path = battery_path.base .. "/" .. battery_path.variants[battery_nr]
+	bat_web_string = bat_web_string .. "\n\n" .. bar.char:rep(bar.length) .. "\n"
+	bat_web_string = bat_web_string .. "\nBattery\n"
+	bat_web_string = bat_web_string .. "${exec cat " .. battery_full_path .. "/status" .. "}"
+	if battery_path.has_health then
+		bat_web_string = bat_web_string .. " / "
+		bat_web_string = bat_web_string .. "${exec cat " .. battery_full_path .. "/health" .. "}"
+	end
+	bat_web_string = bat_web_string .. " -- "
+	bat_web_string = bat_web_string .. "${exec cat " .. battery_full_path .. "/capacity" .. "} %\n"
+	bat_web_string = bat_web_string .. "${execbar cat " .. battery_full_path .. "/capacity" .. "}"
 end
 
 -- [[ Bundle all the needed variables at init ]]
 local init_table = {
-    os_name = pregenerated.release,
-    os_version = pregenerated.release_version,
-    bash_version = "Bash" .. " " .. pregenerated.bash_version,
-    bar = bar.char:rep(bar.length),
-    cpu_name = pregenerated.cpu_name,
-    cpu_graphs = cpu_web_string,
-    mem_graph = mem_web_string,
-    bat_graph = bat_web_string
+	os_name = pregenerated.release,
+	os_version = pregenerated.release_version,
+	bash_version = "Bash" .. " " .. pregenerated.bash_version,
+	bar = bar.char:rep(bar.length),
+	cpu_name = pregenerated.cpu_name,
+	cpu_graphs = cpu_web_string,
+	mem_graph = mem_web_string,
+	bat_graph = bat_web_string
 }
 
 -- [[ Conky text string with local variable support ]]
@@ -194,3 +222,5 @@ local interpolated_string = string_interpolation(raw_string, init_table)
 
 -- [[ Send interpolated string to conky ]]
 conky.text = interpolated_string
+
+-- }}}
--- functions.lua
+++ functions.lua
@@ -1,21 +1,32 @@
 -- [[ Custom functions to be used by conky ]]
 local SEPARATOR = " "
 local MAX_NUMBERS_FREQ = 4
+local STRING_FORMAT = "%.3f"
 
 local CPU_FOLDER = "/sys/devices/system/cpu"
 
 -- [[ String string function ]]
 function string:split(sep)
-    local sep, fields = sep or ":", {}
-    local pattern = string.format("([^%s]+)", sep)
-    self:gsub(pattern, function(c) table.insert(fields, c) end)
+	local sep, fields = sep or ":", {}
+	local pattern = string.format("([^%s]+)", sep)
+	self:gsub(pattern, function(c) table.insert(fields, c) end)
 	return fields
 end
 
 -- [[ Command execution function ]]
 local function run_command (command_string)
-    return io.popen(command_string):read('*a'):sub(1, -2)
+	return io.popen(command_string):read('*a'):sub(1, -2)
 end
+
+-- [[ Mean function ]]
+function average(table)
+	local sum = 0
+	for i = 1, #table do
+		sum = sum + table[i]
+	end
+	return sum/#table
+end
+
 
 -- [[ Functions to be passed to conky ]]
 function conky_get_core(core_number_input)
@@ -26,31 +37,34 @@ function conky_get_core(core_number_input)
 	local freq_now = nil
 
 	-- Acquire the cpu freqs
+	local freq_list = run_command("grep 'cpu MHz' /proc/cpuinfo | awk '{print $4}'"):split("\n")
+
+	-- Check if a average is needed or a specific frequency
 	if core_number == 0 then
-		freq_now = run_command("lscpu | grep \"CPU MHz\" | awk '{print $3}'")
+		freq_now = string.format(STRING_FORMAT, average(freq_list))
 	else
-		freq_now = run_command("grep 'cpu MHz' /proc/cpuinfo | awk '{print $4}'"):split("\n")[core_number]
+		freq_now = freq_list[core_number]
 	end
 
-    -- Check if the method before was successful and if not get frequency through other methods
-    if freq_now == nil or freq_now == "" then
-        if core_number > 0 then
-            freq_now = string.format(
-                "%.3f",
-                tonumber(run_command("cat /sys/devices/system/cpu/cpu" .. core_number - 1 .. "/cpufreq/scaling_cur_freq")) / 1000
-            )
-        end
+	-- Check if the method before was successful and if not get frequency through other methods
+	if freq_now == nil or freq_now == "" then
+		if core_number > 0 then
+			freq_now = string.format(
+				STRING_FORMAT,
+				tonumber(run_command("cat /sys/devices/system/cpu/cpu" .. core_number - 1 .. "/cpufreq/scaling_cur_freq")) / 1000
+			)
+		end
 	end
 
 	-- Add a leading padding if the frequency has not 4 numbers
-    if freq_now ~= nil and freq_now ~= "" then
-        local first_number_len = freq_now:split(".")[1]:len()
-        if first_number_len < MAX_NUMBERS_FREQ then
-            freq_now = string.rep(SEPARATOR, MAX_NUMBERS_FREQ - first_number_len) .. freq_now
-        end
-    else
-        freq_now = "Undefined"
-    end
+	if freq_now ~= nil and freq_now ~= "" then
+		local first_number_len = freq_now:split(".")[1]:len()
+		if first_number_len < MAX_NUMBERS_FREQ then
+			freq_now = string.rep(SEPARATOR, MAX_NUMBERS_FREQ - first_number_len) .. freq_now
+		end
+	else
+		freq_now = "Undefined"
+	end
 
 	-- Return the frequency
 	return freq_now
@@ -82,23 +96,23 @@ end
 
 -- List the processor governor
 function conky_justify_gover(core_number_input, total_size_input)
-    return conky_justify(run_command("cat " .. CPU_FOLDER .. "/cpu" .. tonumber(core_number_input) - 1 .. "/cpufreq/scaling_governor"), total_size_input)
+	return conky_justify(run_command("cat " .. CPU_FOLDER .. "/cpu" .. tonumber(core_number_input) - 1 .. "/cpufreq/scaling_governor"), total_size_input)
 end
 
 -- List the governor of all processors
 function conky_gover_all()
-    -- Check if all the governors are the same
-    local governor_count = run_command("cat " .. CPU_FOLDER .. "/cpu*/cpufreq/scaling_governor | uniq | wc -l")
+	-- Check if all the governors are the same
+	local governor_count = run_command("cat " .. CPU_FOLDER .. "/cpu*/cpufreq/scaling_governor | uniq | wc -l")
 
-    -- Output string
-    local return_string = ""
+	-- Output string
+	local return_string = ""
 
-    -- If they are the same print the first
-    if governor_count == "1" then
-        return_string = run_command("cat " .. CPU_FOLDER .. "/cpu0/cpufreq/scaling_governor")
-    else
-        return_string = "mixed"
-    end
+	-- If they are the same print the first
+	if governor_count == "1" then
+		return_string = run_command("cat " .. CPU_FOLDER .. "/cpu0/cpufreq/scaling_governor")
+	else
+		return_string = "mixed"
+	end
 
-    return return_string
+	return return_string
 end
